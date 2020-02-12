e7f59a0e8abd889530c633998b9bfda54053098a
Rob Moffat <robmoffat@mac.com>
2020-02-12 12:54:11 +0000

    Fixed compile bug

--- src/main/java/org/riskfirst/same/same4j/types/Numbers.java
+++ src/main/java/org/riskfirst/same/same4j/types/Numbers.java
@@ -6,19 +6,19 @@ import org.riskfirst.same.same4j.ReversibleFunction;
 
 public class Numbers {
 
-	public static ReversibleFunction<String, Number> stringToInteger(NumberFormat nf) {
-		return new ReversibleFunction<String, Integer>() {
-
-			@Override
-			public Integer apply(String t) {
-				return nf.parse(t);
-			}
-
-			@Override
-			public String inverse(Integer in) {
-				return nf.par
-			}
-			
-		};
-	}
+//	public static ReversibleFunction<String, Number> stringToInteger(NumberFormat nf) {
+//		return new ReversibleFunction<String, Integer>() {
+//
+//			@Override
+//			public Integer apply(String t) {
+//				return nf.parse(t);
+//			}
+//
+//			@Override
+//			public String inverse(Integer in) {
+//				return nf.par
+//			}
+//			
+//		};
+//	}
 }
